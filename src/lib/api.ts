2d22318e86176b76734cd361b66a6abbdf8b6cce
saul19bautista <lopezbautistasaul12@gmail.com>
2026-03-01 22:33:53 -0600

    cambios de localhost

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://editorial-production.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
